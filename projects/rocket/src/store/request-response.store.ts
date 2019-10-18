e8584771c878c13046855da0e27f762e31624a4f
Chris Humboldt <chrismodem@gmail.com>
2019-10-18 14:34:59 +0800

    Changes to request response store.

--- projects/rocket/src/store/request-response.store.ts
+++ projects/rocket/src/store/request-response.store.ts
@@ -2,15 +2,14 @@
  * @author Chris Humboldt
  */
 
-export const RequestResponse = {
-   200: {status: 200, statusText: 'OK'},
-   201: {status: 201, statusText: 'Created'},
-   202: {status: 201, statusText: 'Accepted'},
-   204: {status: 204, statusText: 'No Content'},
-   400: {status: 400, statusText: 'Bad Request'},
-   401: {status: 401, statusText: 'Unauthorized'},
-   403: {status: 403, statusText: 'Forbidden'},
-   500: {status: 500, statusText: 'Internal Server Error'},
-   502: {status: 502, statusText: 'Bad Gateway'},
-   504: {status: 504, statusText: 'Gateway Timeout'}
-}
+export const RequestResponse = new Map<number, any>()
+   .set(200, {status: 200, statusText: 'OK'})
+   .set(201, {status: 201, statusText: 'Created'})
+   .set(202, {status: 201, statusText: 'Accepted'})
+   .set(204, {status: 204, statusText: 'No Content'})
+   .set(400, {status: 400, statusText: 'Bad Request'})
+   .set(401, {status: 401, statusText: 'Unauthorized'})
+   .set(403, {status: 403, statusText: 'Forbidden'})
+   .set(500, {status: 500, statusText: 'Internal Server Error'})
+   .set(502, {status: 502, statusText: 'Bad Gateway'})
+   .set(504, {status: 504, statusText: 'Gateway Timeout'});
