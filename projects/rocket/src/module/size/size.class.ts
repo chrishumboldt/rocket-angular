860c048f90b74475a47547c1b72dfe2b0a224f09
Chris Humboldt <chrismodem@gmail.com>
2019-11-13 08:45:48 +0800

    Create size.class.ts

---
+++ projects/rocket/src/module/size/size.class.ts
@@ -0,0 +1,25 @@
+/**
+ * @author Chris Humboldt
+ */
+
+import { ValueGenerator } from '../../generator';
+
+export class Size {
+   height: number;
+   width: number;
+
+   constructor(input: any = {}) {
+      this.height = ValueGenerator(this, input, 'height', 0);
+      this.width = ValueGenerator(this, input, 'width', 0);
+   }
+}
+
+export class SizeClasses {
+   height: string;
+   width: string;
+
+   constructor(input: any = {}) {
+      this.height = ValueGenerator(this, input, 'height', '');
+      this.width = ValueGenerator(this, input, 'width', '');
+   }
+}
